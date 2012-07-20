e78f7513aaa547c54a20882f88c0684adfd24eac
craigleehi <craigleehi@gmail.com>
2012-07-20 19:10:07 +0800

    remove unrelated js

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,9 +8,17 @@ import java.util.*;
 import models.*;
 
 public class Application extends Controller {
+	/**
+	 * 首页
+	 */
+	public static void index() {
+		render();
+	}
 
-    public static void index() {
-        render();
-    }
-
+	/**
+	 * 解决方案
+	 */
+	public static void plan() {
+		render();
+	}
 }
